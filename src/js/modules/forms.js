f2403e149ec8e5031d8ae3068e131232a6bf4e86
Evgenij <murzin90@yandex.ru>
2022-07-20 23:59:00 +0700

    Add add calc

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,3 +1,5 @@
+import {postData} from '../services/requests';
+
 // import checkNumInputs from './checkNumInputs';
 
 const forms = () => {
@@ -19,15 +21,6 @@ const forms = () => {
     const path = {
         designer: 'assets/server.php',
         question: 'assets/question.php'
-    };
-
-    const postData = async (url, data) => {
-        let res = await fetch(url, {
-            method: 'POST',
-            body: data
-        });
-
-        return await res.text();
     };
 
     const clearInputs = () => {
